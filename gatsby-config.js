a4e23dfca084e899274b196c1849e55e5b27cb15
ryant4333 <RyanCTaylor95@gmail.com>
2018-11-18 13:31:58 +1000

    Adding typography

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,8 @@
-module.export = {
+module.exports = {
     plugins: [
-        {
-            resolve: `gatsby-plugin-typography`,
-            options: {
-                pathToConfigModule: `src/utils/typography.js`,
-            },
-        }
+        resolve: `gatsby-plugin-typography`,
+        options: {
+            pathToConfigModule: `src/utils/typography.js`,
+        },
     ],
 }
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,7 @@
 import Typography from "typography"
 
 const typography = new Typography({
-    baseFontSize: "50px",
+    baseFontSize: "18px",
     bodyFontFamily: ["georgia", "serif"],
 })
 
