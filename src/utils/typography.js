6ac9e6ea618e6232e764ec91cae89207ae723d5f
ryant4333 <RyanCTaylor95@gmail.com>
2018-11-18 14:02:18 +1000

    Added typography themes

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,8 +1,6 @@
 import Typography from "typography"
+import bootstrapTheme from 'typography-theme-bootstrap'
 
-const typography = new Typography({
-    baseFontSize: "18px",
-    bodyFontFamily: ["georgia", "serif"],
-})
+const typography = new Typography(bootstrapTheme)
 
 export default typography
